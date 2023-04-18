eee3d1b9a55a3b170310c3c40dd76cda659ad633
Lucca Giusti <luccagiusti@gmail.com>
2023-04-18 17:51:18 +0200

    Started to develop the frontend

---
+++ frontend/src/Api.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './Api.css'
+
+type DataType = {
+  id: string;
+  type: string;
+  name: {
+    value: string;
+    type: string;
+  };
+};
+
+const Api: React.FC = () => {
+  const [data, setData] = useState<DataType | null>(null);
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  const handleButtonClick = async (endpoint: string) => {
+    try {
+      const response = await fetch(endpoint);
+      const jsonData = await response.json();
+      setData(jsonData);
+      setShowAnimation(true);
+      setTimeout(() => {
+        setShowAnimation(false);
+      }, 1000);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ display: 'flex'}}>
+        <button onClick={() => handleButtonClick('http://localhost:8080/v2/entities/urn:ngsi-ld:ParkingSpot:Mobility_Hub:1')}>
+          Weight
+        </button>
+        <button onClick={() => handleButtonClick('http://localhost:8080/v2/entities/urn:ngsi-ld:WeatherObserved:Basis:1')}>
+          Weather
+        </button>
+        <button onClick={() => handleButtonClick('http://localhost:8080/v2/entities/urn:ngsi-ld:ParkingSpot:Mobility_Hub:1')}>
+          Energy
+        </button>
+        <button onClick={() => handleButtonClick('http://localhost:8080/v2/entities/urn:ngsi-ld:ParkingSpot:Mobility_Hub:1')}>
+          Parking
+        </button>
+      </div>
+      <div className={`response-box`}>
+        {data && (
+          <pre className={`response ${showAnimation ? 'animate' : ''}`} style={{ color: '#5dc0cf', fontFamily: 'monospace' }}>
+            {JSON.stringify(data, null, 2)}
+          </pre>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Api;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import logo from './assets/logo.png'
+import Api from './Api'
+import './App.css';
+
+const SmartworldContent = () => (
+  <div>
+    <h2>Smartworld Content</h2>
+    <p>This is the Smartworld content.</p>
+  </div>
+);
+
+const SmartDataModelsContent = () => (
+  <div>
+    <h2>Smart Data Models Content</h2>
+    <p>This is the Smart Data Models content.</p>
+  </div>
+);
+
+const APIContent = () => (
+  <div>
+    <h2>API Content</h2>
+    <Api/>
+  </div>
+);
+
+const App: React.FC = () => {
+  const [activeButton, setActiveButton] = useState('Smartworld');
+
+  const handleButtonClick = (buttonName: string) => {
+    setActiveButton(buttonName);
+  };
+
+  const getContent = () => {
+    switch (activeButton) {
+      case 'Smart Data Models':
+        return <SmartDataModelsContent />;
+      case 'API':
+        return <APIContent />;
+      default:
+        return <SmartworldContent />;
+    }
+  };
+
+  return (
+    <>
+      <nav className="navigation-menu">
+        <div className="logo">
+          <img src={logo} alt="My Logo" className='logo' />
+        </div>
+        <div className="buttons">
+          <button className={activeButton === 'Smartworld' ? 'active' : ''} onClick={() => handleButtonClick('Smartworld')}>
+            Smartworld
+          </button>
+          <button className={activeButton === 'Smart Data Models' ? 'active' : ''} onClick={() => handleButtonClick('Smart Data Models')}>
+            Smart Data Models
+          </button>
+          <button className={activeButton === 'API' ? 'active' : ''} onClick={() => handleButtonClick('API')}>
+            API
+          </button>
+        </div>
+      </nav>
+      <div className="content-wrapper">{getContent()}</div>
+    </>
+  );
+};
+
+export default App;
